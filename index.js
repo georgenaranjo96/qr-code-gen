bd96e56a5940f8d2eda7cf215bd00d8951739e89
georgenaranjo96 <georgenaranjo96@gmail.com>
2022-04-15 19:39:08 -0700

    hosttt

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const qrcode = require('qrcode');
 const exp = require("constants");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
